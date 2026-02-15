8284d5884fc3ba90d10b00d31e96e12a1d1a6f73
sarthakjainxyz <jsarthak2007@gmail.com>
2026-02-16 00:47:35 +0530

    feat: actual ping command

--- src/commands/general/ping.ts
+++ src/commands/general/ping.ts
@@ -3,6 +3,8 @@ import {
     ChatInputCommandInteraction,
     EmbedBuilder,
 } from "discord.js";
+import { db } from "@/db";
+import { sql } from "drizzle-orm";
 
 export const command = {
     data: new SlashCommandBuilder()
@@ -10,11 +12,39 @@ export const command = {
         .setDescription("Ping Pong!"),
 
     async execute(interaction: ChatInputCommandInteraction) {
+        const start = Date.now();
         await interaction.deferReply();
+
+        const wsPing = interaction.client.ws.ping;
+        const apiLatency = Date.now() - start;
+        let dbLatency: number | string;
+
+        try {
+            const dbStart = Date.now();
+            await db.execute(sql`select 1`);
+            dbLatency = Date.now() - dbStart;
+        } catch {
+            dbLatency = "Error";
+        }
 
         const embed = new EmbedBuilder()
             .setTitle("🏓 Pong!")
             .setColor(0x7040ff)
+            .addFields(
+                {
+                    name: "Discord",
+                    value: `${apiLatency}ms`,
+                    inline: true,
+                },
+                {
+                    name: "Database",
+                    value:
+                        typeof dbLatency === "number"
+                            ? `${dbLatency}ms`
+                            : "Unavailable",
+                    inline: true,
+                },
+            )
             .setTimestamp();
 
         await interaction.editReply({ embeds: [embed] });
